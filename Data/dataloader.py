169f987c1dcb6818dc8cf5e68a9e1e8d0328e8b6
Minjee Seo <79445201+Minjee-Seo@users.noreply.github.com>
2022-09-14 15:41:12 +0900

    Update dataloader.py

--- Data/dataloader.py
+++ Data/dataloader.py
@@ -1,3 +1,34 @@
+import torch
 from torch.utils.data import Dataset, DataLoader
 
-loader = DataLoader(dataset = dataset, batch_size = 64, shuffle = True)
+BATCH_SIZE = 1
+
+# 임의의 데이터셋 생성
+
+x = [[0,0,0], [0,0,1], [0,1,0],
+    [1,0,0], [0,1,1], [1,1,0],
+    [1,0,1], [1,1,1]]
+
+y = [0, 1, 2, 3, 4, 5, 6, 7]
+
+x = torch.Tensor(x).float()
+y = torch.Tensor(y).long()
+
+# Dataset class 이용해 dataset 정의
+
+class Data(Dataset):
+    def __init__(self):
+        self.x_data = x
+        self.y_data = y
+        
+    def __len__(self):
+        return len(self.x_data)
+    
+    def __getitem__(self, idx):
+        return self.x_data[idx], self.y_data[idx]
+      
+# 인스턴스 생성
+
+dataset = Data()
+
+loader = DataLoader(dataset = dataset, batch_size = BATCH_SIZE, shuffle = True)
